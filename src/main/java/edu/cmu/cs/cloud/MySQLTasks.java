6f9822d036f3a976397d3b25885c2e767f8292b4
Lawrence Han <lawrench@andrew.cmu.edu>
2021-03-14 17:52:51 -0400

    Q9 revision 1

--- src/main/java/edu/cmu/cs/cloud/MySQLTasks.java
+++ src/main/java/edu/cmu/cs/cloud/MySQLTasks.java
@@ -274,8 +274,7 @@ public class MySQLTasks {
      */
     private static void q9() {
         String sql = "SELECT user_id FROM cool = (SELECT MAX(cool) FROM " +
-                "reviews) AND user_id IN (SELECT user_id FROM " +
-                "tips)";
+                "reviews) AND user_id IN tips";
         executeDataManipulationQuery(sql);
     }
 
