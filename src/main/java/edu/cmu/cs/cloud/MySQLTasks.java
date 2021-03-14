a21825fefe535a4cd2ed0b8aba7c0f4c2c3d9634
Lawrence Han <lawrench@andrew.cmu.edu>
2021-03-14 18:30:30 -0400

    Q9 revision 7

--- src/main/java/edu/cmu/cs/cloud/MySQLTasks.java
+++ src/main/java/edu/cmu/cs/cloud/MySQLTasks.java
@@ -273,10 +273,13 @@ public class MySQLTasks {
      * You are only allowed to edit the sql.
      */
     private static void q9() {
-        String sql = "SELECT reviews.user_id FROM reviews " +
-                " WHERE reviews.cool = (SELECT MAX(reviews.cool) FROM reviews)"+
-                "INNER JOIN tips WHERE tips.user_id = reviews.user_id " ;
-
+        String sql =
+//                "SELECT reviews.user_id FROM reviews " +
+//                "INNER JOIN tips WHERE tips.user_id = reviews.user_id " +
+//                " WHERE reviews.cool = (SELECT MAX(reviews.cool) FROM reviews)";
+                "SELECT tips.user_id from tips WHERE tips.user_id=" +
+                        "(SELECT user_id FROM reviews" +
+                        " WHERE cool = (SELECT MAX(cool) FROM reviews))";
         executeDataManipulationQuery(sql);
     }
 
