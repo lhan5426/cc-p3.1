b02592a6f3bd042615036d39b25f8d85e6cc94f3
Lawrence Han <lawrench@andrew.cmu.edu>
2021-03-14 23:47:54 -0400

    Q10.9 with partition no typo 9

--- src/main/java/edu/cmu/cs/cloud/MySQLTasks.java
+++ src/main/java/edu/cmu/cs/cloud/MySQLTasks.java
@@ -296,7 +296,7 @@ public class MySQLTasks {
         String sql =
                 "SELECT city, ROUND(AVG(stars)) AS avg_stars" +
                 "FROM businesses " +
-                "GROUP BY city ASC " +
+                "GROUP BY city" +
                 "ORDER BY avg_stars DESC, city ASC " +
                 "LIMIT 3";
         executeDataManipulationQuery(sql);
