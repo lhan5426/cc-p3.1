4635d89ef3e98c4271263771ef43e03e194e5d7d
Lawrence Han <lawrench@andrew.cmu.edu>
2021-03-14 20:12:45 -0400

    Q10.42 averages

--- src/main/java/edu/cmu/cs/cloud/MySQLTasks.java
+++ src/main/java/edu/cmu/cs/cloud/MySQLTasks.java
@@ -294,9 +294,9 @@ public class MySQLTasks {
         String sql = "SELECT businesses.user_id, businesses.stars, " +
                 "businesses.avg_num, businesses.city " +
                 "FROM (SELECT city, AVG(stars) as avg_num " +
-                       "FROM TableName " +
+                       "FROM businesses " +
                        "GROUP BY city) AG " +
-                "JOIN TableName businesses ON businesses.city = AG.city";
+                "JOIN businesses businesses ON businesses.city = AG.city";
 //                "(SELECT AVG(stars) AS avg_num, city " +
 //                "FROM businesses " +
 //                "GROUP BY city) grouped " +
