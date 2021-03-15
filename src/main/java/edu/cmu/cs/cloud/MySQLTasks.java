df9db4c2528021a58167fc26ae26eb799d4fbc65
Lawrence Han <lawrench@andrew.cmu.edu>
2021-03-14 21:13:17 -0400

    Q10.62 ???

--- src/main/java/edu/cmu/cs/cloud/MySQLTasks.java
+++ src/main/java/edu/cmu/cs/cloud/MySQLTasks.java
@@ -307,7 +307,9 @@ public class MySQLTasks {
                 "SELECT businesses.city FROM businesses " +
                 "WHERE businesses.city IN " +
                 "(SELECT businesses.city, AVG(businesses.stars) 'avg_num' " +
-                "FROM businesses  " +
+                "FROM businesses " +
+                        "LEFT OUTER JOIN businesses" +
+                        "ON businesses.city = businesses.city" +
                 "GROUP BY businesses.city)" +
                 "ORDER BY avg_num DESC, businesses.city DESC " +
                 "LIMIT 3";
