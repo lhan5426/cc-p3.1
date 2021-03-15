7f52e5298729404f83f649dfa45692d64eef0672
Lawrence Han <lawrench@andrew.cmu.edu>
2021-03-14 20:21:00 -0400

    Q10.45 averages

--- src/main/java/edu/cmu/cs/cloud/MySQLTasks.java
+++ src/main/java/edu/cmu/cs/cloud/MySQLTasks.java
@@ -291,8 +291,7 @@ public class MySQLTasks {
      * You are only allowed to edit the sql.
      */
     private static void q10() {
-        String sql = "SELECT businesses.business_id, businesses.stars, " +
-                " businesses.city " +
+        String sql = "SELECT businesses.stars, businesses.city " +
                 "FROM (SELECT city, AVG(stars) as avg_num " +
                        "FROM businesses " +
                        "GROUP BY city) AG " +
